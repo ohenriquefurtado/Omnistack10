c36941b7a0da19b5eb00869fb616bfe7ba2c4798
henriquefurtadob@gmail.com <henriquefurtadob@gmail.com>
2020-01-16 12:54:49 -0300

    web

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const routes = require('./routes')
 
 const app = express()
@@ -7,8 +8,11 @@ const app = express()
 mongoose.connect('mongodb+srv://henrique:barros@cluster0-eigqc.mongodb.net/week10?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
 })
 
+app.use(cors({}))
 app.use(express.json())
 app.use(routes)
 
